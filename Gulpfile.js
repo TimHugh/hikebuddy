e7cfdd022acacc71831cf72ceb48a69c334a3896
Tim Heuett <tim.heuett@gmail.com>
2016-07-31 11:30:48 -0700

    add sass compilation to gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,6 +6,7 @@ const gulp       = require('gulp'),
       gutil      = require('gulp-util'),
       livereload = require('gulp-livereload'),
       ngAnnotate = require('gulp-ng-annotate'),
+      sass       = require('gulp-sass'),
       source     = require('vinyl-source-stream'),
       sourcemaps = require('gulp-sourcemaps'),
       uglify     = require('gulp-uglify')
@@ -13,7 +14,7 @@ const gulp       = require('gulp'),
 
 const Config = require('./gulp-config.json');
 
-gulp.task('javascript', ['clean'], () => {
+gulp.task('javascript', ['clean:javascript'], () => {
   var b = browserify({
     entries: Config.source + '/js/app.js',
     debug: true
@@ -27,22 +28,33 @@ gulp.task('javascript', ['clean'], () => {
       .pipe(ngAnnotate())
       .pipe(uglify())
       .on('error', gutil.log)
-    .pipe(sourcemaps.write(Config.destination))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest(Config.destination))
     .pipe(livereload());
 });
 
-gulp.task('copy-index', ['clean'], () => {
+gulp.task('css', ['clean:css'], () => {
+  gulp.src(Config.source + '/sass/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(Config.destination));
+});
+
+gulp.task('copy-index', ['clean:index'], () => {
   return gulp.src(Config.source + '/index.html')
     .pipe(gulp.dest(Config.destination));
 });
 
 gulp.task('clean', () => del([ Config.destination ]));
+gulp.task('clean:css', () => del([ Config.destination + '**/*.css' ]));
+gulp.task('clean:javascript', () => del([ Config.destination + '**/*.js' ]));
+gulp.task('clean:index', () => del([ Config.destination + '/index.html' ]));
 
 gulp.task('watch', ['build'], () => {
   livereload.listen();
-  gulp.watch(Config.source + '/**/*', ['build']);
+  gulp.watch(Config.source + '/**/*.js', ['javascript']);
+  gulp.watch(Config.source + '/**/*.css', ['css']);
+  gulp.watch(Config.source + '/index.html', ['copy-index']);
 });
 
-gulp.task('build', ['copy-index', 'javascript']);
+gulp.task('build', ['copy-index', 'css', 'javascript']);
 gulp.task('default', ['build']);
--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,5 @@ const angular = require('angular');
 module.exports = angular.module('hikebuddy', []).
 
   controller('ctrl', ($scope) => {
-    console.log('controller');
     $scope.title = "This is a title!";
   });
